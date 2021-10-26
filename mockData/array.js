5493388562bd0770def3a5a9d0c3c36e4e8f536c
Luiz Carlos Froes <luizfroes@gmail.com>
2021-10-27 00:47:41 +0100

    Added the search options Arrays

---
+++ mockData/array.js
@@ -0,0 +1,38 @@
+const dieatryOptions = [
+  "Gluten Free",
+  "Ketogenic",
+  "Vegetarian",
+  "Lacto-Vegetarian",
+  "Ovo-Vegetarian",
+  "Vegan",
+  "Pescetarian",
+  "Paleo",
+];
+
+const intolerancesOptions = [
+  "Dairy",
+  "Egg",
+  "Gluten",
+  "Peanut",
+  "Seafood",
+  "Sesame",
+  "Shellfish",
+  "Soy",
+  "Sulfite",
+  "Tree Nut",
+];
+
+const cuisinesOptions = [
+  "American",
+  "British",
+  "Chinese",
+  "French",
+  "Indian",
+  "Italian",
+  "Japanese",
+  "Mediterranean",
+  "Mexican",
+  "Middle Eastern",
+  "Spanish",
+  "Thai",
+];
