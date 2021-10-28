16d57266dc52ccd415e94a13932b3fd7bc950c3c
Luiz Carlos Froes <luizfroes@gmail.com>
2021-10-28 01:25:07 +0100

    added the search options arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,48 @@ $(document).ready(function () {
   );
 });
 
+//search options arrays
+const dieatryOptions = [
+  "Gluten Free",
+  "Ketogenic",
+  "Vegetarian",
+  "Lacto-Vegetarian",
+  "Ovo-Vegetarian",
+  "Vegan",
+  "Pescetarian",
+  "Paleo",
+];
+
+const intolerancesOptions = [
+  "Dairy",
+  "Egg",
+  "Gluten",
+  "Peanut",
+  "Seafood",
+  "Sesame",
+  "Shellfish",
+  "Soy",
+  "Sulfite",
+  "Tree Nut",
+];
+
+const cuisinesOptions = [
+  "American",
+  "British",
+  "Chinese",
+  "French",
+  "Indian",
+  "Italian",
+  "Japanese",
+  "Mediterranean",
+  "Mexican",
+  "Middle Eastern",
+  "Spanish",
+  "Thai",
+];
+
+//construct the search bar
+
 //get input value from user
 const handleSearch = (event) => {
   event.preventDefault();
