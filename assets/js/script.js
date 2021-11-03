0a167b105d435a09d2757d91f9186adeabb34398
liana valeria <liana.valeria15@gmail.com>
2021-11-03 21:11:01 +0000

    added function to transform data and render random recipes cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,39 @@ const handleSearch = (event) => {
   window.location.assign("../../menu.html");
 };
 
+//transform random recipe data
+const constructRandomRecipeObject = (data) => {
+  const callback = (each) => {
+    return {
+      id: each.id,
+      image: each.image,
+      title: each.title,
+    };
+  };
+  return data.recipes.map(callback);
+};
+
+//construct random recipes cards
+const constructRandomRecipeCards = (data) => {
+  //construct each random recipe card
+  const callback = (each) => {
+    const randomRecipeCard = `<div class="high-rated-recipe">
+      <figure class="image is-128x128">
+        <img class="is-rounded" src=${each.image}>
+        <h3>${each.title}</h3>
+      </figure>
+    </div>`;
+    //append random recipe card to random recipes container
+    $("#random-recipes-container").append(randomRecipeCard);
+  };
+  data.forEach(callback);
+};
+
+//on page load execute functions:
 const handleLoad = () => {
+  // render random recipes cards
+  const randomRecipesData = constructRandomRecipeObject(mockRandomRecipesData);
+  constructRandomRecipeCards(randomRecipesData);
   const onClick = (event) => {
     event.preventDefault();
     const searchInput = $("#search-input").val();
