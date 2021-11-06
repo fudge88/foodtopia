fe07e0e575bc3ea33d3cf2c03feb97fc9b22a5f0
Natasha Mann <natasha.s.mann@gmail.com>
2021-11-06 10:40:32 +0000

    updated number from api url

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -146,7 +146,7 @@ const constructApiUrl = function (baseUrl, searchOptions) {
   if (searchOptions.cuisines.length) {
     cuisinesQuery = `cuisines=${searchOptions.cuisines.join(",")}`;
   }
-  const url = `${baseUrl}?query=${searchQuery}&${dietQuery}&${intolerancesQuery}&${cuisinesQuery}&addRecipeNutrition=true&apiKey=${API_KEY}&number=1`;
+  const url = `${baseUrl}?query=${searchQuery}&${dietQuery}&${intolerancesQuery}&${cuisinesQuery}&addRecipeNutrition=true&apiKey=${API_KEY}&number=12`;
   return url;
 };
 
